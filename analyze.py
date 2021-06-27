cc79057816179f15bb17455ea8d8e1b05965d23c
Maya Tagunova <maya.tagunova@gmail.com>
2021-06-27 17:15:56 +0300

    Implemented pull requests functions

--- analyze.py
+++ analyze.py
@@ -8,6 +8,7 @@ import requests
 import json
 import collections
 import copy
+from datetime import datetime, timedelta
 
 script_name = os.path.basename(__file__)
 user = 'MTagunova'
@@ -44,7 +45,65 @@ def show_authors(owner: str, repo: str, branch: str):
     authors = sorted(authors.items(), key=lambda kv: kv[1], reverse=True)
 
     for item in authors[:30]:
-        print(f'{item[0]}  {item[1]}')
+        print(f'{item[0]}: {item[1]}')
+
+
+def filter_by_date(entries: typing.List[typing.Any],
+                   start_date: typing.Optional[str] = None, end_date: typing.Optional[str] = None) \
+        -> typing.List[typing.Any]:
+    results = []
+    if start_date is None or end_date is None:
+        return entries
+    start_date = f'{start_date}T00:00:00Z'
+    end_date = f'{end_date}T23:59:59Z'
+
+    for item in entries:
+        if item['created_at'] < start_date:
+            break
+        if start_date < item['created_at'] < end_date:
+            results.append(item)
+
+    return results
+
+
+def filter_old(entries: typing.List[typing.Any]) -> typing.List[typing.Any]:
+    results = []
+    one_month_ago = (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    for item in entries:
+        if item['created_at'] < one_month_ago:
+            results.append(item)
+
+    return results
+
+
+def show_pull_requests(owner: str, repo: str, branch: str,
+                       start_date: typing.Optional[str] = None, end_date: typing.Optional[str] = None):
+    payload = {'per_page': 100, 'page': 1, 'base': branch}
+    if start_date is not None and end_date is not None:
+        payload['since'] = f'{start_date}T00:00:00Z'
+        payload['until'] = f'{end_date}T23:59:59Z'
+
+    results_open = filter_by_date(
+                   get_full_list(f'{base_url}/repos/{owner}/{repo}/pulls', payload), start_date, end_date)
+    payload['state'] = 'closed'
+    results_closed = filter_by_date(
+                    get_full_list(f'{base_url}/repos/{owner}/{repo}/pulls', payload), start_date, end_date)
+    results_old = filter_old(results_open)
+
+    print(f'Open pull requests: {len(results_open)}')
+    print(f'Closed pull requests: {len(results_closed)}')
+    print(f'Old pull requests: {len(results_old)}')
+
+
+def analyze(url, branch, start_date: typing.Optional[str] = None, end_date: typing.Optional[str] = None):
+    repo_info = url.split('/')
+    owner = repo_info[-2]
+    repo = repo_info[-1]
+
+    show_authors(owner, repo, branch)
+    print('...')
+    show_pull_requests(owner, repo, branch, start_date, end_date)
 
 
 def main():
