3bf9c356e570a0067f600f8163f4f43c32546633
MayaTagunova <maya.tagunova@gmail.com>
2021-06-28 08:42:33 +0300

    Implemented date validation

--- analyze.py
+++ analyze.py
@@ -127,21 +127,34 @@ def analyze(options: typing.Dict[str, typing.Any]):
     show_issues(options)
 
 
+def valid_date(date):
+    try:
+        return datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        msg = f''''{date}' is not a valid date'''
+        raise argparse.ArgumentTypeError(msg)
+
+
 def main():
     auth_info = 'For unauthenticated requests, the API rate limit allows for up to 60 requests per hour. ' \
                 'If that is not sufficient, you can make up to 5,000 requests per hour using an OAuth token. ' \
                 'Provide the username and token using the -u and -t keys'
     parser = argparse.ArgumentParser(epilog=auth_info)
     parser.add_argument('-b', '--branch', default='master', help='Branch')
-    parser.add_argument('-s', '--start_date', default='1970-01-01',
+    parser.add_argument('-s', '--start_date', type=valid_date, default='1970-01-01',
                         help='Start date as YYYY-MM-DD')
-    parser.add_argument('-e', '--end_date', default='9999-12-31',
+    parser.add_argument('-e', '--end_date', type=valid_date, default='9999-12-31',
                         help='End date as YYYY-MM-DD')
     parser.add_argument('-u', '--username', help='Username')
     parser.add_argument('-t', '--token', help='OAuth token')
     parser.add_argument('URL', help='Repository URL')
 
-    options = vars(parser.parse_args())
+    try:
+        options = vars(parser.parse_args())
+    except argparse.ArgumentTypeError as err:
+        print(err)
+        return 2
+
     options['start_date'] = f'''{options['start_date']}T00:00:00Z'''
     options['end_date'] = f'''{options['end_date']}T23:59:59Z'''
 
